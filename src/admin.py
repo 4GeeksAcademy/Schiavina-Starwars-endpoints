14a3e6fef198d0dcd0d2b63675a919fc42d47163
maxischiavina <123894494+maxischiavina@users.noreply.github.com>
2023-05-26 19:06:24 +0000

    primera parte ok

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, Usuario, Personaje, Planeta, Vehicle, Personaje_favorito, Planeta_favorito, Vehicle_favorito
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,7 +10,14 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Usuario, db.session))
+    admin.add_view(ModelView(Personaje, db.session))
+    admin.add_view(ModelView(Planeta, db.session))
+    admin.add_view(ModelView(Vehicle, db.session))
+    admin.add_view(ModelView(Personaje_favorito, db.session))
+    admin.add_view(ModelView(Vehicle_favorito, db.session))
+    admin.add_view(ModelView(Planeta_favorito, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -2,18 +2,131 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # name = db.Column(db.String(250), nullable=False)
+    # email = db.Column(db.String(250), nullable=False)
+    # password = db.Column(db.String(250), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Usuario %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "email": self.email, 
             # do not serialize the password, its a security breach
         }
+
+
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(250), nullable=False)
+    # eye_color = db.Column(db.String(250), nullable=False)
+    # hair_color = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Personaje %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye,
+            "hair_color": self.hair,
+            # do not serialize the password, its a security breach
+        }
+    
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(250), nullable=False)
+    # population = db.Column(db.String(250), nullable=False)
+    # terrain = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(250), nullable=False)
+    # crew = db.Column(db.String(250), nullable=False)
+    # vehicle_class = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "crew": self.crew,
+            "vehicle_class": self.vehicleclass,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+class Personaje_favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # usuario_id = db.Column(db.Integer, ForeignKey('usuario.id'))
+    # personaje_id = db.Column(db.Integer, ForeignKey('personaje.id'))
+    # usuario = relationship(Usuario)
+    # personaje = relationship(Personaje)
+
+def __repr__(self):
+        return '<Personaje_favorito %r>' % self.id
+
+def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuarioid,
+            "personaje_id": self.personajeid,
+        }
+
+
+class Vehicle_favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    # vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+    # usuario = relationship(Usuario)
+    # vehicle = relationship(Vehicle)
+
+def __repr__(self):
+        return '<Vehicle_favorito %r>' % self.id
+
+def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuarioid,
+            "vehicle_id": self.vehicleid,
+        }
+
+
+class Planeta_favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    # planeta_id = db.Column(db.Integer, db.ForeignKey('planeta.id'))
+    # usuario = relationship(Usuario)
+    # planeta = relationship(Planeta)
+def __repr__(self):
+        return '<Vehicle_favorito %r>' % self.id
+
+def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuarioid,
+            "planeta_id": self.planetaid,
+        }
+
+    
